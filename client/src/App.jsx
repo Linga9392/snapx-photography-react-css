858f473bf8caeedc4ff993c578579d4ec0996c6e
linga9392 <venkatalingaiah@paithacs.com>
2026-07-15 18:25:30 +0530

    fix: filename case

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Categories from "./components/Categories";
-import PreviousContests from './components/PreviousContests'
+import PreviousContests from "./components/previousContests"
 import Pricing from "./components/Pricing";  
 import Footer from "./components/Footer";
 import "./App.css";
@@ -12,9 +12,9 @@ function App() {
       <Navbar />
       <Hero />
       <Categories />
-      <PreviousContests />  {/* 1. Previous Contests first */}
-      <Pricing />           {/* 2. Pricing tarvatha */}
-      <Footer />            {/* 3. Footer last */}
+      <PreviousContests />  
+      <Pricing />           
+      <Footer />            
     </div>
   );
 }
